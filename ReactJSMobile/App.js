2d20047e14aec1efeaa808f3af8cd732813ace07
VU Tuan Trung <trung.lespace@gmail.com>
2019-02-01 16:28:17 +0100

    [UPDATE] Adding file

---
+++ ReactJSMobile/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ComponentHead  from './src/Component.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <ComponentHead/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
